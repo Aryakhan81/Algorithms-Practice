1f976fe41bd14d05716b394631f1ed6e2e9b752a
Arya Gharib <40894911+Aryakhan81@users.noreply.github.com>
2022-11-30 19:52:46 -0800

    Add in functional tree implementation

--- binary-trees.cpp
+++ binary-trees.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
+class Tree {
+    public:
+        Tree();
+        Tree(string name);
+        Tree(Tree* l, Tree* r);
+        Tree(Tree* l, Tree* r, string name);
+        ~Tree();
+        Tree* left;
+        Tree* right;
+        string label;
+};
+
+Tree::Tree() {
+    this->left = nullptr;
+    this->right = nullptr;
+    ostringstream oss;
+    oss << this;
+    this->label = "Tree@" + oss.str();
+}
+
+Tree::Tree(string name) {
+    this->left = nullptr;
+    this->right = nullptr;
+    this->label = name;
+}
+
+Tree::Tree(Tree* l, Tree* r) {
+    this->left = l;
+    this->right = r;
+    ostringstream oss;
+    oss << this;
+    this->label = "Tree@" + oss.str();
+}
+
+Tree::Tree(Tree* l, Tree* r, string name) {
+    this->left = l;
+    this->right = r;
+    this->label = name;
+}
+
+Tree::~Tree() {
+    delete this->left;
+    delete this->right;
+}
+
 int main() {
-    
+    Tree t = Tree("A");
+    t.left = new Tree("B");
+    t.right = new Tree("C");
+    t.right->right = new Tree();
+    cout << t.right->right->label << endl;
+    return 0;
 }
